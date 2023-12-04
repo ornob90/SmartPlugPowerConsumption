c174b08414fd055c8df3144cbc24151a6985980c
ornob90 <oornob49@gmail.com>
2023-12-04 10:42:36 +0600

    side bar complete

--- src/pages/Dashboard/Sidebar.jsx
+++ src/pages/Dashboard/Sidebar.jsx
@@ -1,12 +1,56 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FaBoltLightning } from "react-icons/fa6";
+import { IoMdWarning } from "react-icons/io";
+import { FaPowerOff } from "react-icons/fa";
+import { FaChartBar } from "react-icons/fa";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
 const Sidebar = () => {
+  const navItem = [
+    {
+      name: "Trend",
+      icon: <FaChartBar />,
+    },
+    {
+      name: "Current",
+      icon: <FaBoltLightning />,
+    },
+    {
+      name: "Power",
+      icon: <FaPowerOff />,
+    },
+    {
+      name: "Voltage",
+      icon: <IoMdWarning />,
+    },
+  ];
+
+  const [activeNav, setActiveNav] = useState("Trend");
+
   return (
     <div className="w-[20%] min-h-screen bg-[#293541]">
       <div className="h-[13%] bg-[#E57E31] text-white font-bold flex  pl-4 items-center text-2xl">
-        {" "}
         Admin Panel
       </div>
+      <ul className="flex flex-col pt-10 gap-5 text-white font-bold text-md ">
+        {navItem.map(({ name, icon }) => (
+          <li
+            onClick={() => setActiveNav(name)}
+            key={name}
+            className={`py-4 px-4 flex justify-between items-center gap-4 border-b-2  mx-[5%] border-b-[#36434C] cursor-pointer ${
+              activeNav === name
+                ? "text-white  bg-[#34404C] rounded-md"
+                : "text-[#727C85]"
+            }`}
+          >
+            <p className="flex justify-center items-center gap-2">
+              {icon} {name}
+            </p>
+            <p className={`${activeNav === name ? "" : "hidden"}`}>
+              <FaArrowRightFromBracket />
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
